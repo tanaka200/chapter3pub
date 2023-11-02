51a1b1b362835f2c9d673674fc09cb629bf65d69
tanaka200 <131841182+tanaka200@users.noreply.github.com>
2023-11-02 13:43:00 +0900

    Initial commit

---
+++ ledon.py
@@ -0,0 +1,17 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+# GPIO.BCMを指定するとGPIO番号モードで指定
+# GPIO.BOARDを指定するとpin番号モードで指定
+GPIO.setmode(GPIO.BCM)
+
+# GPIO14を出力に設定
+GPIO.setup(14, GPIO.OUT)
+
+# GPIO14にHighを出力
+GPIO.output(14, GPIO.HIGH)
+
+# 3sスリープ
+time.sleep(3)
+
+GPIO.cleanup()
