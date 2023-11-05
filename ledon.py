9c9c42491f06498009531fb6290cd1f6ff4296cf
tanaka200 <131841182+tanaka200@users.noreply.github.com>
2023-11-06 02:40:47 +0900

    a

--- ledon.py
+++ ledon.py
@@ -2,16 +2,22 @@
 import RPi.GPIO as GPIO
 
 # GPIO.BCMを指定するとGPIO番号モードで指定
-# GPIO.BOARDを指定するとpin番号モードで指定
 GPIO.setmode(GPIO.BCM)
 
 # GPIO14を出力に設定
 GPIO.setup(14, GPIO.OUT)
 
-# GPIO14にHighを出力
-GPIO.output(14, GPIO.HIGH)
+try:
+    while True:
+        # GPIO14にHighを出力（LED点灯）
+        GPIO.output(14, GPIO.HIGH)
+        time.sleep(1)  # 1秒待つ
 
-# 3sスリープ
-time.sleep(3)
+        # GPIO14にLowを出力（LED消灯）
+        GPIO.output(14, GPIO.LOW)
+        time.sleep(1)  # 1秒待つ
+
+except KeyboardInterrupt:
+    pass
 
 GPIO.cleanup()
